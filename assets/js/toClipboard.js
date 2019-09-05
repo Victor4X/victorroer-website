999ac1983fbec49278d73d2d8975d14f64427816
Victor4X <viggo00@hotmail.com>
2019-09-05 14:27:00 +0200

    base64 support?

--- assets/js/toClipboard.js
+++ assets/js/toClipboard.js
@@ -1,7 +1,14 @@
 var query = window.location.href
 var url = new URL(query);
 var s = url.searchParams.get("s");
-console.log(s);
+var data = url.searchParams.get("data");
+console.log(s, data);
+
+if (s !== null && data == "base64"){
+    s = Base64.decode('ZGFua29nYWk=');
+    console.log(s, data);
+    }
+
 if (s !== null){
     document.getElementById("copytext").innerHTML = "\""+s+"\" was copied to the clipboard!";
     navigator.clipboard.writeText(s);
